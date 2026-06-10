6fbd7871b116408aa496766bc8100948c20a969f
soni-tee <soni-tee@users.noreply.github.com>
2026-06-10 13:09:02 +0300

    Migrate 3D heart into React Three Fiber component

--- src/components/Canvas3D.tsx
+++ src/components/Canvas3D.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment } from '@react-three/drei'
+import { OrbitControls, Environment, Stars } from '@react-three/drei'
 import { Suspense, ReactNode } from 'react'
+import { HeartModel } from './HeartModel'
 
 interface Canvas3DProps {
   children?: ReactNode
@@ -10,35 +11,40 @@ interface Canvas3DProps {
 
 export function Canvas3D({ children }: Canvas3DProps) {
   return (
-    <div className="w-full h-full relative bg-neutral-900">
-      <Canvas shadows camera={{ position: [0, 0, 10], fov: 45 }}>
+    <div className="w-full h-full relative bg-[#040609]">
+      <Canvas shadows camera={{ position: [0, 0.5, 9.5], fov: 45 }}>
+        <color attach="background" args={['#040609']} />
+        <fogExp2 attach="fog" args={['#040609', 0.055]} />
+        
         <Suspense fallback={null}>
-          <Environment preset="city" />
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
+          {/* Lighting from PoC */}
+          <ambientLight color="#1a0812" intensity={3.5} />
+          <directionalLight color="#ffcca0" intensity={4.5} position={[4, 7, 6]} castShadow />
+          <directionalLight color="#5577ff" intensity={2} position={[-5, 2, 4]} />
+          <directionalLight color="#cc2000" intensity={3} position={[0, -5, -6]} />
+          
+          <pointLight color="#ff1800" intensity={10} distance={4.5} position={[0, -0.2, 0.3]} />
+          <pointLight color="#ff6644" intensity={3} distance={5} position={[-0.3, 1.8, 0.5]} />
+          
+          {/* Stars */}
+          <Stars radius={140} depth={50} count={3500} factor={4} saturation={0} fade speed={1} />
           
           {children}
           
-          {/* Default placeholder if no children are provided */}
-          {!children && (
-            <mesh>
-              <boxGeometry args={[2, 2, 2]} />
-              <meshStandardMaterial color="#ef4444" wireframe />
-            </mesh>
-          )}
+          {!children && <HeartModel />}
           
           <OrbitControls 
             makeDefault 
             enableDamping 
-            dampingFactor={0.05} 
-            minDistance={2} 
-            maxDistance={50} 
+            dampingFactor={0.06} 
+            minDistance={3.5} 
+            maxDistance={22} 
+            autoRotate
+            autoRotateSpeed={0.45}
+            target={[0, 0, 0]}
           />
         </Suspense>
       </Canvas>
-      <div className="absolute top-4 left-4 text-white/50 text-sm pointer-events-none">
-        3D Viewport
-      </div>
     </div>
   )
 }
